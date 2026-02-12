89d3e25614a58b4b032e80b1f9d38e70ed1339ba
victtorsantos2t <victtor.santos2t@gmail.com>
2026-02-12 15:34:23 -0400

    Remover config eslint incompatível

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,6 @@ const nextConfig: NextConfig = {
   /* config options here */
   typescript: {
     ignoreBuildErrors: true, // Útil para deploys iniciais com versões experimentais
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
   }
 };
 
