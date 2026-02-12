936b5243a08a25a53d7d2c73a0c6b2562e1796e8
victtorsantos2t <victtor.santos2t@gmail.com>
2026-02-12 16:24:17 -0400

    Fix: Garantindo rotas raiz e de teste

---
+++ app/test/page.tsx
@@ -0,0 +1,8 @@
+export default function TestPage() {
+    return (
+        <div style={{ padding: '50px', fontFamily: 'sans-serif' }}>
+            <h1>Test Page Working</h1>
+            <p>If you see this, the Vercel routing is working correctly.</p>
+        </div>
+    );
+}
